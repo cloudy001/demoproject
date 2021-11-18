a65cf70c6e2137d9cb57469588a9d33fd8292a2f
Said Belbasri <sbelbasri@gmail.com>
2021-11-18 23:20:35 +0100

    update java TestClass

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,6 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Hallo World der DevOps Engoineering....its funny....");
-        System.out.println("bla bla bla");
+        System.out.println("bla bla bladvdfdfgfd");
     }
 }
