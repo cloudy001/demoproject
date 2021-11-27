31ff6c9b6e428f22109fdc89e0b6dce5130809e2
Said Belbasri <sbelbasri@gmail.com>
2021-11-27 13:09:28 +0100

    my first devops pipeline

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,6 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Hallo World der DevOps Engoineering....its funny....");
-        System.out.println("bla bla bladvdfdretretertfgfd") ;
+        System.out.println("bla bla bladvdfdretretertfwfwfefgfd") ;
     }
 }
